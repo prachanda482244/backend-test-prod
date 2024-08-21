0741199ffaca5f4b3be79b0f9d81f712424df75b
=Prachanda Rana <=>
2024-08-21 11:46:01 +0545

    host

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-import express, { urlencoded } from "express";
-import cors from "cors";
-import { config } from "dotenv";
-config();
-const PORT = process.env.PORT || 6000;
+import express from "express";
+import crypto from "crypto";
+
 const app = express();
-app.use(cors({ origin: "*", withCredentials: true }));
+const SHOPIFY_SECRET = "6d1e7a324e5bc2c0583b0ca056237a4d";
 
-app.use(express.json({ limit: "20mb" }));
-app.use(urlencoded({ extended: true, limit: "20mb" }));
+app.use(express.json()); // Parse JSON request bodies
 
-app.post("/get-console-data", (req, res) => {
-  res.status(200).send(req.body);
+async function verifyShopifyRequest(req, res, next) {
+  const hmacHeader = req.get("X-Shopify-Hmac-Sha256");
+
+  console.log("Received X-Shopify-Hmac-Sha256:", hmacHeader);
+
+  if (!hmacHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const body = JSON.stringify(req.body); // Get the raw body as a string
+  const hmac = crypto
+    .createHmac("sha256", SHOPIFY_SECRET)
+    .update(body, "utf8")
+    .digest("base64");
+
+  const isValidRequest = crypto.timingSafeEqual(
+    Buffer.from(hmac),
+    Buffer.from(hmacHeader)
+  );
+
+  if (!isValidRequest) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  next();
+}
+
+app.get("/", verifyShopifyRequest, (req, res) => {
+  res.status(200).json({ message: "OK" });
 });
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Json response", data: [], success: true });
+
+app.post("/webhook_compliance/shopify/", verifyShopifyRequest, (req, res) => {
+  res.status(200).json({ message: "OK" });
 });
 
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
